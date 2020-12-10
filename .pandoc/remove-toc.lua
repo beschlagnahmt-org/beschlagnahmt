fe9731fdca98c8edb287ada42ef0aded4310e20b
Stefan Leibfarth <github@leibfarth.org>
2020-12-10 11:42:40 +0100

    Simplify filter

--- .pandoc/remove-toc.lua
+++ .pandoc/remove-toc.lua
@@ -2,13 +2,13 @@ return {
   {
     Str = function (elem)
       if elem.text == "Inhalt" then
-        return pandoc.Emph {pandoc.Str " "}
+        return pandoc.Str ""
       elseif elem.text == [[{:.no_toc}]] then
-        return pandoc.Emph {pandoc.Str " "}
+        return pandoc.Str ""
       elseif elem.text == [[{:toc}]] then
-        return pandoc.Emph {pandoc.Str " "}
+        return pandoc.Str ""
       elseif elem.text == [[TOC]] then
-        return pandoc.Emph {pandoc.Str " "}
+        return pandoc.Str ""
       else
         return elem
       end
