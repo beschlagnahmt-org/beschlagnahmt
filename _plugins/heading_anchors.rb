ebdd4051d82fdee13eb144113d41a8b289693911
Stefan Leibfarth <stefan+github@leibfarth.org>
2026-01-10 14:12:14 +0000

    Anker eingefügt

---
+++ _plugins/heading_anchors.rb
@@ -0,0 +1,11 @@
+Jekyll::Hooks.register [:pages, :posts], :post_render do |doc|
+  # Sucht nach h1-h6 mit ID und fügt einen Anker-Link vor das schließende Tag ein
+  doc.output.gsub!(/(<h([1-6]) id="([^"]+)">)(.*?)(<\/h\2>)/) do
+    opening_tag = $1
+    id = $3
+    text = $4
+    closing_tag = $5
+    
+    "#{opening_tag}#{text} <a href='##{id}' class='anchor-link'>#</a>#{closing_tag}"
+  end
+end
